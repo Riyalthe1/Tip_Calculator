b95b8217001528f66b71fe9e5c3d7b7f5703d748
Riyal Riyan <riyel1422@gmail.com>
2024-02-19 17:52:28 +0600

    added the header

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -4,14 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,15 +53,88 @@ fun MyApp() {
 
     }
 }
+
 @Composable
-fun TipCalculator () {
+fun TipCalculator() {
     Column(modifier = Modifier.fillMaxWidth()) {
 
     }
 }
-@Composable
-fun TotalHeader(){
-Surface(modifier = Modifier.fillMaxWidth().padding(12.dp)){
 
+@Preview(showBackground = true)
+@Composable
+fun TotalHeader(amount: Float = 0f) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        color = colorResource(id = R.color.light_blue),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Total Per Person",
+                style = TextStyle(color = Color.Black),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "$${amount}",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp
+                )
+            )
+        }
+    }
 }
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun UserInputArea(amount: String, amountChange: (String) -> Unit) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp), shape = RoundedCornerShape(12.dp), tonalElevation = 12.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = amount,
+                onValueChange = { amountChange.invoke(it) },
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = {
+                    Text(
+                        text = "Enter your Amount"
+                    )
+                },
+                keyboardOptions = KeyboardOptions(
+                    autoCorrect = true,
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ), keyboardActions = KeyboardActions(onDone = {
+                    keyboardController?.hide()
+                })
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Row (modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp), verticalAlignment = Alignment.CenterVertically){
+Text(text = "Split", style = MaterialTheme.typography.bodyMedium)
+                Spacer(modifier = Modifier.fillMaxWidth(.5f))
+            }
+        }
+    }
 }
